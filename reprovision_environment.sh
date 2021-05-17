426338ac628953cc478c0b26a73877282a0ee66d
Emilien Macchi <emilien@redhat.com>
2021-05-17 10:12:58 -0400

    First commit

---
+++ reprovision_environment.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# pull all repos
+# - secrets if running as a user
+# - dev-install
+
+# destroy environment if it exists
+
+# create server
+
+# wait for SSH to be available
+
+# decrypt "openshift" user password
+
+# run dev-install with config
